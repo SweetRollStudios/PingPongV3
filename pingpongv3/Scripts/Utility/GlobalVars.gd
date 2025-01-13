0d31e20795ce8327fce319042667644268d50573
SweetRollStudios <osrsgamerkid@gmail.com>
2025-01-13 11:58:41 -0600

    changes

--- pingpongv3/Scripts/Utility/GlobalVars.gd
+++ pingpongv3/Scripts/Utility/GlobalVars.gd
@@ -1,7 +1,7 @@
 extends Node
 
-@onready var player1Color : Color = Color.WHITE
-@onready var player2Color : Color = Color.WHITE
+@onready var player1Color : Color = Color.BLUE
+@onready var player2Color : Color = Color.RED
 @onready var player1Name : String = ''
 @onready var player2Name : String = ''
 # Called when the node enters the scene tree for the first time.
